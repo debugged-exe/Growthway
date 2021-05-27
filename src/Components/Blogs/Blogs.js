4c46be205e18114bf7af0a4b5af137af4ae686ae
ShabbirK52 <ShabbirKachwala@users.noreply.github.com>
2021-05-27 14:06:42 +0530

    Blogs component completed

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import image from './cryptocurrency-market.jpg';
-import {BlogsList} from './BlogsList.js';
+import {BsChevronDoubleDown } from "react-icons/bs";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BlogsList } from './BlogsList.js';
 import 'tachyons';
+import './Blog.scss';
 
 const Blogs = () => {
+
+	const[setItems]=useState([]);
+	const [visible, setVisible] = useState(3);
+	const [len, setLen] = useState(0);
+
+	useEffect(() => {
+		fetch('https://fierce-wave-93667.herokuapp.com/completed')
+			.then(response => response.json())
+			.then(resp => {
+				if (resp[0].title) {
+					setItems(resp);
+					setLen(resp.length);
+					console.log(resp.length);
+				}
+			})
+			.catch(err => {
+				console.log(err)
+
+			})
+	}, [])
+
+	const showMoreItems = () => {
+		setVisible((prev) => prev + 3);
+	}
+
 	return (
 		<div>
-      <h1>Blogs</h1>
+			<h1>Blogs</h1>
 			<div>
-			{
-				BlogsList.map((item)=>{
-					return(
-						<div style={{background: `url(${item.image}) no-repeat center center`,height:'75vh', backgroundSize: 'cover'}}
-						className=" center">
-							<div className="white" style={{backgroundColor:'rgba(0,0,0,0.5)',width:'55vw',height:'50vh'}}>
-								<h1>{item.heading}</h1>
-								<p>{item.description}</p>
+				{
+					BlogsList.slice(0, visible).map((item) => {
+						return (
+							<div style={{ background: `url(${item.image}) no-repeat center center`, backgroundSize: 'cover' }}
+								className=" center blog-container">
+								<p className="white blog-id">
+									{item.id}
+								</p>
+								<div className="blog-content white" style={{ backgroundColor: 'rgba(0,0,0,0.5)', position: 'relative' }}>
+									<h1 className="blog-heading">{item.heading}</h1>
+									<p>{item.description}</p>
+								</div>
 							</div>
-						</div>
-					)
-				})
-			}
+						);
+					})
+				}
 			</div>
-
+			<h5 onClick={showMoreItems} className={`center showmore mt5 ${visible >= len ? 'hide' : ''}`} style= {{textAlign: 'center'}}>Show More</h5>
+			<BsChevronDoubleDown className={` arrowdown code ${visible >= len ? 'hide' : ''}`} size="2rem" style= {{width:'100%', textAlign: 'center'}}/>
 		</div>
 	)
 }
--- src/Components/Blogs/BlogsList.js
+++ src/Components/Blogs/BlogsList.js
@@ -2,15 +2,45 @@ import image1 from './cryptocurrency-market.jpg';
 
 export const BlogsList=
 [{
-  id:1,
+  id:'01',
   heading:'Heading',
   description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
   image:image1
 },
 {
-  id:2,
+  id:'02',
   heading:'Heading',
-  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  image:image1
+
+},{
+  id:'03',
+  heading:'Heading',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  image:image1
+
+},{
+  id:'04',
+  heading:'Heading',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  image:image1
+
+},{
+  id:'05',
+  heading:'Heading',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  image:image1
+
+},{
+  id:'06',
+  heading:'Heading',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
+  image:image1
+
+},{
+  id:'07',
+  heading:'Heading',
+  description:'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. WikipediaIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia',
   image:image1
 
 }];
