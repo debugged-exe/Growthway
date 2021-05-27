4684aed2487fe174622d2b70c1a0e64db7369c77
harshika6 <harshikasmishra@gmail.com>
2021-05-27 11:02:20 +0530

    blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import image from './cryptocurrency-market.jpg';
+import {BlogsList} from './BlogsList.js';
 import 'tachyons';
 
 const Blogs = () => {
 	return (
 		<div>
       <h1>Blogs</h1>
+			<div>
+			{
+				BlogsList.map((item)=>{
+					return(
+						<div style={{background: `url(${item.image}) no-repeat center center`, backgroundSize: 'cover'}}
+						className=" center">
+							<h1 className="white">{item.heading}</h1>
+							<img src={item.image}/>
+						</div>
+					)
+				})
+			}
+			</div>
+
 		</div>
 	)
 }
 
 export default Blogs;
-	// <div style={{background: `url(${item.image}) no-repeat center center`, backgroundSize: 'cover'}}
-	// className=" center mv5 flex flex-column justify-center">
---
+++ src/Components/Blogs/BlogsList.js
@@ -0,0 +1,16 @@
+import image1 from './cryptocurrency-market.jpg';
+
+export const BlogsList=
+[{
+  id:1,
+  heading:'Heading',
+  description:'',
+  image:image1
+},
+{
+  id:2,
+  heading:'Heading',
+  description:'',
+  image:image1
+
+}];
