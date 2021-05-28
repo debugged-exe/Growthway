5af779ad884ec083125f9318b5c60777be155578
ShabbirK52 <ShabbirKachwala@users.noreply.github.com>
2021-05-28 15:46:25 +0530

    Testimonials done.

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -1,34 +1,40 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'tachyons';
-// import './Testimonial.scss';
+import { TestimonialData } from './TestimonialData.js';
 import Carousel from 'react-elastic-carousel';
 import './style.scss';
 
 const Testimonial = () => {
-	return (
-		<div className="testimonials">
-    <p className="tf pt3 pl5"
-    style={{fontSize:'3.5rem',fontWeight:'500'}}>
-      Testimonial's </p>
-	  <p className="tf pt3 pl5"
-    style={{fontSize:'1rem',fontWeight:'300'}}>From our early days, we've been providing reliable service to our clientele.<br/>
-We've had the honor being the firm of choice of the following corporations:</p>
-		<Carousel itemsToShow={3}>
-		<div className="mt5 pa5 bg-white client-card">
-			<h1> Client's name </h1>
-			<p>
-			From our early days, we've been providing reliableservice to our clientele.
-			We've had the honor being the firm of choice of thefollowing corporations:
-			</p>
-		</div>
-		<div>2</div>
-		<div>3</div>
-		<div>4</div>
-		<div>5</div>
-		<div>6</div>
-		</Carousel>
-		</div>
-	)
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 850, itemsToShow: 2 },
+        { width: 1150, itemsToShow: 3 },
+        { width: 1450, itemsToShow: 3 },
+        { width: 1750, itemsToShow: 3 },
+    ]
+
+    return (
+        <div className="testimonials" >
+            <p className="testimonial-heading">
+                Testimonial's </p>
+            <p className=" testimonial-quote">
+                From our early days, we've been providing reliable service to our clientele.<br />
+                We've had the honor being the firm of choice of the following corporations:</p>
+            <Carousel breakPoints={breakPoints}>
+                {
+                    TestimonialData.map((item) => {
+                        return (
+                            <div className="bg-white client-card">
+                                <h3>{item.client}</h3>
+                                <p>{item.description}</p>
+                            </div>
+                        )
+                    })
+                }
+            </Carousel>
+        </div>
+    )
 }
 
 export default Testimonial;
---
+++ src/Components/Testimonial/TestimonialData.js
@@ -0,0 +1,19 @@
+export const TestimonialData=[
+    {
+      client:"Shabbir Kachwala",
+      description:"Great service thats on time per agreed upon schedule.The communication staff and the service technicians are always willing to work around the customer's schedule.Hats off to them! "
+    },
+    {
+      client:"Harshika Mishra",
+      description:"I can recommend debugged.exe team with confidence as I belive them to be specialists that offer a quick turnaround and high quality of service at an affordable price. "
+    },
+    {
+      client:"Tejas Machkar",
+      description:"I can recommend debugged.exe team with confidence as I belive them to be specialists that offer a quick turnaround and high quality of service at an affordable price. "
+  
+    },
+    {
+      client:"Sumedh Ovhal",
+      description:"Great service thats on time per agreed upon schedule.The communication staff and the service technicians are always willing to work around the customer's schedule.Hats off to them! "
+    }];
+  
