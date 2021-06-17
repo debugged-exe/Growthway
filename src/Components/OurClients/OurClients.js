28ac34d83e85356308edf711211296ade4b631fe
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2021-06-17 22:35:57 +0530

    Ourservices

---
+++ src/Components/OurClients/OurClients.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './OurClients.scss';
+
+
+const testimonial = [
+	{
+		
+	}
+]
+
+const OurClients = () => {
+	return (
+		<div className="our-services">
+			<h2>Our Clients</h2>
+			<div className="testimonial-grid">
+				
+			</div>
+		</div>
+	)
+}
+
+export default OurClients;
