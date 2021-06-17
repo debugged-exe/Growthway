a50feb665ef3aa55f59c9d060beee16bf0b5b37b
harshika6 <harshikasmishra@gmail.com>
2021-06-17 17:33:18 +0530

    here we go again

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -15,38 +15,48 @@ const Blogs = () => {
 	}
 
 	return (
-		<div>
-			<h1 className="tc">Blogs</h1>
-			<div className="blog-item-container">
-				{
-					BlogsList.slice(0,visible)
-					.map((item,index) => {
-						return(
-							<div className="blog-item">
-								<div>
-									<img src={item.image} alt="hehe" style={{width: '100%', objectFit: 'cover'}}/>
-								</div>
-								<div className="blog-item-header">
-									<h3 className="">{item.heading}</h3>
-									<span style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center'}}>
-										<BsCalendar color={'green'} size={'1.5rem'} />
-										<p className="mh2" style={{color: 'green', fontWeight: 'bold', fontSize: '1.3rem'}}>{item.date}</p>
-									</span>
-								</div>
-								<div>
-									<p className="blog-item-desc">{item.description}</p>
-								</div>
-							</div>
-						);
-					})
-				}
-			</div>
-			<div className="pointer" onClick={showMoreItems}>
-				<p className={`center ${visible >= len ? 'hide' : ''}`} style= {{textAlign: 'center'}}>Show More</p>
-				<BsChevronDoubleDown className={` arrowdown code ${visible >= len ? 'hide' : ''}`} size="2rem" style= {{width:'100%', textAlign: 'center'}}/>
-			</div>
+		<div className="blog-container">
+		   <div className="item">1</div>
+			 <div className="item">2</div>
+			 <div className="item">3</div>
 		</div>
+
 	)
 }
 
 export default Blogs;
+
+
+
+
+// <div>
+// 	<h1 className="tc">Blogs</h1>
+// 	<div className="blog-item-container">
+// 		{
+// 			BlogsList.slice(0,visible)
+// 			.map((item,index) => {
+// 				return(
+// 					<div className="blog-item">
+// 						<div>
+// 							<img src={item.image} alt="hehe" style={{width: '100%', objectFit: 'cover'}}/>
+// 						</div>
+// 						<div className="blog-item-header">
+// 							<h3 className="">{item.heading}</h3>
+// 							<span style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center'}}>
+// 								<BsCalendar color={'green'} size={'1.5rem'} />
+// 								<p className="mh2" style={{color: 'green', fontWeight: 'bold', fontSize: '1.3rem'}}>{item.date}</p>
+// 							</span>
+// 						</div>
+// 						<div>
+// 							<p className="blog-item-desc">{item.description}</p>
+// 						</div>
+// 					</div>
+// 				);
+// 			})
+// 		}
+// 	</div>
+// 	<div className="pointer" onClick={showMoreItems}>
+// 		<p className={`center ${visible >= len ? 'hide' : ''}`} style= {{textAlign: 'center'}}>Show More</p>
+// 		<BsChevronDoubleDown className={` arrowdown code ${visible >= len ? 'hide' : ''}`} size="2rem" style= {{width:'100%', textAlign: 'center'}}/>
+// 	</div>
+// </div>
--- src/Pages/BlogPage/BlogPage.js
+++ src/Pages/BlogPage/BlogPage.js
@@ -8,7 +8,7 @@ import './BlogPage.scss';
 
 const BlogPage = () => {
 	return (
-		<div className="blog-page bg-dark-gray">
+		<div className="blog-page">
 			<div className="blog-page-header">
 				<h1 className="">Our Latest <span style={{color: 'green'}}>News</span></h1>
 				<p className="blog-page-para">Deerunt sit ut tempor non ut enim deserunt proident sint elit nostrud non veniam commodo mollit nisi nulla ut dolor irure nulla reprehenderit laborum in est in sed do sunt non.</p>
