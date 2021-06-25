07a207a6d1df86ef6987df9101b361d3737d52d4
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-06-25 23:55:44 +0530

    changed submit button

--- src/Components/Faqs/QuestionInput.js
+++ src/Components/Faqs/QuestionInput.js
@@ -46,9 +46,9 @@ render(){
       <p className="calc-head fw6" style={{fontSize:'35px'}}>Got few doubts? Send them up</p>
       <div>
 
-      <div className="pa2 mr2" >
+      <div className="pr2 mr3" >
       <form onSubmit={(event) => this.handleForm(event)} className="button-div-faq">
-          <textarea type="text" className="mr2-ns br2"
+          <textarea type="text" className=""
           style={{
               outline:"none",
               border:"none",
@@ -59,7 +59,7 @@ render(){
           placeholder="Your Doubts"
           required
           />
-            <button type="submit" className="fw6 bg-white black pa2 ph3 f5 grow pointer" style={{borderRadius:'8px',border:'none'}}>
+            <button type="submit" className="fw6 bg-white black pa2 ph3 f5 grow pointer faq-hover" style={{height:"41px", border:'none'}}>
             Submit 
             </button>
         </form>
